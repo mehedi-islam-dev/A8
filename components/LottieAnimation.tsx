5f4eb39793637559b1abb366a95c77b907f08692
mehedi-islam-dev <mehediislam0173@gmail.com>
2026-05-04 14:21:28 +0600

    fix - lottie animation

--- components/LottieAnimation.tsx
+++ components/LottieAnimation.tsx
@@ -22,7 +22,7 @@ export default function LottieAnimation() {
   return (
     <div className="w-full flex items-center justify-center p-4 transform transition-transform hover:scale-105 duration-700">
       <DotLottieReact
-        src="https://lottie.host/801a61b8-6a56-4c31-97b7-66a9ba86bc51/J30V0v9nJw.lottie"
+        src="/Vacation-mode.lottie"
         loop={true}
         autoplay={true}
         className="w-full max-w-2xl h-auto drop-shadow-2xl" 
